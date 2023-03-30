b6b6f9a6fdb1d5fe39d1f3ee1bce8e2e63b52e43
sai kalyan bhagavathula <saikalyan.bhagavathula@opsramp.com>
2023-03-30 13:32:34 +0530

    changes related to packaging deb/rpm

--- build/tracing-deb/script.sh
+++ build/tracing-deb/script.sh
@@ -13,9 +13,9 @@ sed -i "/^Architecture/s/:.*$/: ${architecture}/g" tracing/DEBIAN/control
 # Updating the files
 mkdir -p tracing/opt/opsramp/tracing-proxy/bin
 mkdir -p tracing/opt/opsramp/tracing-proxy/conf
-cp ../../config_complete.toml tracing/opt/opsramp/tracing-proxy/conf/config_complete.toml
-cp ../../rules_complete.toml tracing/opt/opsramp/tracing-proxy/conf/rules_complete.toml
-go build ../../cmd/tracing-proxy/main.go
+cp config_complete.toml tracing/opt/opsramp/tracing-proxy/conf/config_complete.toml
+cp rules_complete.toml tracing/opt/opsramp/tracing-proxy/conf/rules_complete.toml
+go build -o ../../cmd/tracing-proxy/main ../../cmd/tracing-proxy/main.go
 cp ../../cmd/tracing-proxy/main tracing/opt/opsramp/tracing-proxy/bin/tracing-proxy
 go build configure.go
 cp configure tracing/opt/opsramp/tracing-proxy/bin/configure
