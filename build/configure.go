94adbdb22a17d11c16e7f16db6c1a27af87ef0d3
saikalyan-bhagavathula <103252261+saikalyan-bhagavathula@users.noreply.github.com>
2023-04-03 14:33:50 +0530

    packaging related changes (#5)

---
+++ build/configure.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	var configFile []byte
+	var fileContent string
+	var err error
+
+	configFile, err = ioutil.ReadFile("/opt/opsramp/tracing-proxy/conf/config_complete.yaml")
+
+	api := flag.String("A", "", "API To Send Data")
+	key := flag.String("K", "", "Opsramp Key")
+	secret := flag.String("S", "", "Opsramp Secret")
+	tenant := flag.String("T", "", "Opsramp TenantID")
+	metricsAPI := flag.String("M", "", "API To Send Metrics Data")
+	flag.Parse()
+
+	fileContent = string(configFile)
+	fileContent = strings.ReplaceAll(fileContent, "<OPSRAMPAPI>", *api)
+	fileContent = strings.ReplaceAll(fileContent, "<OPSRAMPMETRICSAPI>", *metricsAPI)
+	fileContent = strings.ReplaceAll(fileContent, "<KEY>", *key)
+	fileContent = strings.ReplaceAll(fileContent, "<SECRET>", *secret)
+	fileContent = strings.ReplaceAll(fileContent, "<TENANTID>", *tenant)
+
+	if err = ioutil.WriteFile("/opt/opsramp/tracing-proxy/conf/config_complete.yaml", []byte(fileContent), 0666); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if _, err := exec.Command("systemctl", "start", "tracing-proxy").Output(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Tracing-Proxy Started Successfully")
+}
