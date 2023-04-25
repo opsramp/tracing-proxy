262dbaf09777221e0aece1b291606b8a9991556b
Lokesh-Balla <Lokesh-Balla@users.noreply.github.com>
2023-04-25 09:49:09 +0530

    updating configure.go to default to api for metrics and traces enpoint when they are not set

--- build/vm/configure.go
+++ build/vm/configure.go
@@ -3,31 +3,52 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
 )
 
 func main() {
-	var configFile []byte
-	var fileContent string
-	var err error
+	configFile, err := os.ReadFile("/opt/opsramp/tracing-proxy/conf/config_complete.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	configFile, err = os.ReadFile("/opt/opsramp/tracing-proxy/conf/config_complete.yaml")
-
-	api := flag.String("A", "", "API To Send Data")
-	key := flag.String("K", "", "Opsramp Key")
-	secret := flag.String("S", "", "Opsramp Secret")
-	tenant := flag.String("T", "", "Opsramp TenantID")
-	metricsAPI := flag.String("M", "", "API To Send Metrics Data")
+	api := flag.String("A", "", "API for Authorization")
+	key := flag.String("K", "", "OpsRamp Key")
+	secret := flag.String("S", "", "OpsRamp Secret")
+	tenant := flag.String("T", "", "OpsRamp TenantID")
+	tracesAPI := flag.String("B", "", "API to Sent Traces (Defaults to Authorization API specified using -A flag if not set)")
+	metricsAPI := flag.String("M", "", "API To Send Metrics (Defaults to Authorization API specified using -A flag if not set)")
 	flag.Parse()
 
-	fileContent = string(configFile)
-	fileContent = strings.ReplaceAll(fileContent, "<OPSRAMPAPI>", *api)
-	fileContent = strings.ReplaceAll(fileContent, "<OPSRAMPMETRICSAPI>", *metricsAPI)
+	if *api == "" {
+		log.Fatal("api cant be empty, please specify using -A flag")
+	}
+	if *key == "" {
+		log.Fatal("key cant be empty, please specify using -K flag")
+	}
+	if *secret == "" {
+		log.Fatal("secret cant be empty, please specify using -S flag")
+	}
+	if *tenant == "" {
+		log.Fatal("tenant cant be empty, please specify using -T flag")
+	}
+	if *tracesAPI == "" {
+		*tracesAPI = *api
+	}
+	if *metricsAPI == "" {
+		*metricsAPI = *api
+	}
+
+	fileContent := string(configFile)
+	fileContent = strings.ReplaceAll(fileContent, "<OPSRAMP_API>", *api)
+	fileContent = strings.ReplaceAll(fileContent, "<OPSRAMP_TRACES_API>", *tracesAPI)
+	fileContent = strings.ReplaceAll(fileContent, "<OPSRAMP_METRICS_API>", *metricsAPI)
 	fileContent = strings.ReplaceAll(fileContent, "<KEY>", *key)
 	fileContent = strings.ReplaceAll(fileContent, "<SECRET>", *secret)
-	fileContent = strings.ReplaceAll(fileContent, "<TENANTID>", *tenant)
+	fileContent = strings.ReplaceAll(fileContent, "<TENANT_ID>", *tenant)
 
 	if err = os.WriteFile("/opt/opsramp/tracing-proxy/conf/config_complete.yaml", []byte(fileContent), 0666); err != nil {
 		fmt.Println(err)
