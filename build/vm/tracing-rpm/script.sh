141e2728a2f6e416de76c6dcd00ba6551529b7f9
Lokesh-Balla <Lokesh-Balla@users.noreply.github.com>
2023-05-23 15:10:17 +0530

    github action for building RPM package

--- build/vm/tracing-rpm/script.sh
+++ build/vm/tracing-rpm/script.sh
@@ -3,49 +3,51 @@
 yum -y install rpmdevtools
 rpmdev-setuptree
 
+BUILD_DIR="."
+
 if [ "$IS_GITHUB_ACTION" = "true" ]; then
-  cd build/vm/tracing-deb
+  BUILD_DIR="build/vm/tracing-deb"
 fi
 
 Release=$(uname -m)
-sed -i "/^\%define release/s/^.*$/\%define release     ${Release}/g" tracing-proxy.spec
+sed -i "/^\%define release/s/^.*$/\%define release     ${Release}/g" $BUILD_DIR/tracing-proxy.spec
 # $1 is a version of the package
 Version=$1
 if [[ -z "$Version" ]]; then
   Version=$VERSION_TAG
 fi
-sed -i "/^\%define version/s/^.*$/\%define version     ${Version}/g" tracing-proxy.spec
+sed -i "/^\%define version/s/^.*$/\%define version     ${Version}/g" $BUILD_DIR/tracing-proxy.spec
 
 # Building a static binaries
 CGO_ENABLED=0 \
   GOOS=linux \
   GOARCH=amd64 \
   go build -ldflags "-X main.BuildID=${Version}" \
-  -o tracing-proxy \
-  ../../../cmd/tracing-proxy
+  -o $BUILD_DIR/tracing-proxy \
+  $BUILD_DIR/../../../cmd/tracing-proxy/main.go
 
 CGO_ENABLED=0 \
   GOOS=linux \
   GOARCH=amd64 \
   go build -ldflags "-X main.BuildID=${Version}" \
-  -o configure \
-  ../configure.go
+  -o $BUILD_DIR/configure \
+  $BUILD_DIR/../configure.go
 
 package_name="tracing-proxy-${1}"
-mkdir -p ${package_name}/opt/opsramp/tracing-proxy/bin/
-cp -r ../package_directories/* ${package_name}
-mv configure ${package_name}/opt/opsramp/tracing-proxy/bin/configure
-mv tracing-proxy ${package_name}/opt/opsramp/tracing-proxy/bin/tracing-proxy
+mkdir -p $BUILD_DIR/${package_name}/opt/opsramp/tracing-proxy/bin/
+cp -r $BUILD_DIR/../package_directories/* $BUILD_DIR/${package_name}
+mv $BUILD_DIR/configure $BUILD_DIR/${package_name}/opt/opsramp/tracing-proxy/bin/configure
+mv $BUILD_DIR/tracing-proxy $BUILD_DIR/${package_name}/opt/opsramp/tracing-proxy/bin/tracing-proxy
 
-tar -czvf ${package_name}.tar.gz ${package_name}
+tar -czvf $BUILD_DIR/${package_name}.tar.gz $BUILD_DIR/${package_name}
 
-mv ${package_name}.tar.gz /root/rpmbuild/SOURCES/
-cp tracing-proxy.spec /root/rpmbuild/SPECS/tracing-proxy.spec
+mv $BUILD_DIR/${package_name}.tar.gz /root/rpmbuild/SOURCES/
+cp $BUILD_DIR/tracing-proxy.spec /root/rpmbuild/SPECS/tracing-proxy.spec
 
 rpmbuild -ba --clean /root/rpmbuild/SPECS/tracing-proxy.spec
 
 echo "***** rpm package can be found in /root/rpmbuild/RPMS/x86_64/<package-name> ****"
 
 # CleanUp
-rm -rf ${package_name}
-rm -rf configure tracing-proxy
+rm -rf $BUILD_DIR/${package_name}
+rm -rf $BUILD_DIR/configure $BUILD_DIR/tracing-proxy
