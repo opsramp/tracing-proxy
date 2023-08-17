c19cd908a69549c8bea56bb7af8fef4e31b93989
lokesh.balla <lokesh.balla@hpe.com>
2023-08-17 11:54:03 +0530

    updating hpe deployments to support tini and multiregion

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+CLUSTERINFO_PATH='/config/data/infra_clusterinfo.json'
 ELASTICACHE_PATH='/config/data/infra_elasticache.json'
 
 #  Sample Format for ${ELASTICACHE_PATH}
 #  {
 #    "elasticache": {
-#      "host": "master.testing-non-cluster.89rows.usw2.cache.amazonaws.com",
-#      "host_ro": "replica.testing-non-cluster.89rows.usw2.cache.amazonaws.com",
+#      "host": "some_url",
+#      "host_ro": "some_url",
 #      "port": 6379,
 #      "username": "test_user",
 #      "password": "xxxxxx",
@@ -14,12 +15,35 @@ ELASTICACHE_PATH='/config/data/infra_elasticache.json'
 #      "cluster_mode": "false"
 #    }
 #  }
+#  Sample Format for ${ELASTICACHE_PATH} in case of multi region
+#  {
+#    "elasticache": [{
+#      "us-west-2": {
+#        "host": "some_url",
+#        "host_ro": "some_url",
+#        "port": 6379,
+#        "username": "",
+#        "password": "xxxxxx",
+#        "tls_mode": true,
+#        "cluster_mode": "false"
+#      },
+#      "us-east-2": {
+#        "host": "some_url",
+#        "host_ro": "some_url",
+#        "port": 6379,
+#        "username": "",
+#        "password": "xxxxxx",
+#        "tls_mode": true,
+#        "cluster_mode": "false"
+#      }
+#    }]
+#  }
 
-OPSRAMP_CREDS_PATH='/config/data/config_opsramp-tracing-proxy-creds.json'
+OPSRAMP_CREDS_PATH='/config/data/config_tracing-proxy.json'
 
 #  Sample Format for ${OPSRAMP_CREDS_PATH}
 #  {
-#    "opsramp-tracing-proxy-creds": {
+#    "tracing-proxy": {
 #      "traces_api": "test.opsramp.net",
 #      "metrics_api": "test.opsramp.net",
 #      "auth_api": "test.opsramp.net",
@@ -37,11 +61,38 @@ TRACE_PROXY_RULES='/etc/tracing-proxy/final_rules.yaml'
 cp /etc/tracing-proxy/config.yaml ${TRACE_PROXY_CONFIG}
 cp /etc/tracing-proxy/rules.yaml ${TRACE_PROXY_RULES}
 
+if [ -r ${CLUSTERINFO_PATH} ]; then
+
+  CURRENT_REGION=$(jq <${CLUSTERINFO_PATH} -r .clusterinfo.CURRENT_REGION)
+  READ_WRITE_REGION=$(jq <${CLUSTERINFO_PATH} -r .clusterinfo.READ_WRITE_REGION)
+
+  while [ "${CURRENT_REGION}" != "${READ_WRITE_REGION}" ]; do sleep 30; done
+fi
+
 if [ -r ${ELASTICACHE_PATH} ]; then
   # check if the configuration is a object or array
   TYPE=$(jq <${ELASTICACHE_PATH} -r .elasticache | jq 'if type=="array" then true else false end')
   if [ "${TYPE}" = true ]; then
-    echo "implement me"
+
+    if [ -r ${CLUSTERINFO_PATH} ]; then
+
+      CURRENT_REGION=$(jq <${CLUSTERINFO_PATH} -r .clusterinfo.CURRENT_REGION)
+
+      CREDS=$(jq <${ELASTICACHE_PATH} -r .elasticache[0].\""${CURRENT_REGION}"\")
+
+      REDIS_HOST=$(echo "${CREDS}" | jq -r '(.host)+":"+(.port|tostring)')
+      REDIS_USERNAME=$(echo "${CREDS}" | jq -r .username)
+      REDIS_PASSWORD=$(echo "${CREDS}" | jq -r .password)
+      REDIS_TLS_MODE=$(echo "${CREDS}" | jq -r .tls_mode | tr '[:upper:]' '[:lower:]')
+
+      sed -i "s/<REDIS_HOST>/${REDIS_HOST}/g" ${TRACE_PROXY_CONFIG}
+      sed -i "s/<REDIS_USERNAME>/${REDIS_USERNAME}/g" ${TRACE_PROXY_CONFIG}
+      sed -i "s/<REDIS_PASSWORD>/${REDIS_PASSWORD}/g" ${TRACE_PROXY_CONFIG}
+      sed -i "s/<REDIS_TLS_MODE>/${REDIS_TLS_MODE}/g" ${TRACE_PROXY_CONFIG}
+
+    fi
+
+
   else
     REDIS_HOST=$(jq <${ELASTICACHE_PATH} -r '(.elasticache.host)+":"+(.elasticache.port|tostring)')
     REDIS_USERNAME=$(jq <${ELASTICACHE_PATH} -r .elasticache.username)
@@ -57,12 +108,12 @@ fi
 
 if [ -r ${OPSRAMP_CREDS_PATH} ]; then
 
-  TRACES_API=$(jq <${OPSRAMP_CREDS_PATH} -r .opsramp-tracing-proxy-creds.traces_api)
-  METRICS_API=$(jq <${OPSRAMP_CREDS_PATH} -r .opsramp-tracing-proxy-creds.metrics_api)
-  AUTH_API=$(jq <${OPSRAMP_CREDS_PATH} -r .opsramp-tracing-proxy-creds.auth_api)
-  KEY=$(jq <${OPSRAMP_CREDS_PATH} -r .opsramp-tracing-proxy-creds.key)
-  SECRET=$(jq <${OPSRAMP_CREDS_PATH} -r .opsramp-tracing-proxy-creds.secret)
-  TENANT_ID=$(jq <${OPSRAMP_CREDS_PATH} -r .opsramp-tracing-proxy-creds.tenant_id)
+  TRACES_API=$(jq <${OPSRAMP_CREDS_PATH} -r '."tracing-proxy".traces_api')
+  METRICS_API=$(jq <${OPSRAMP_CREDS_PATH} -r '."tracing-proxy".metrics_api')
+  AUTH_API=$(jq <${OPSRAMP_CREDS_PATH} -r '."tracing-proxy".auth_api')
+  KEY=$(jq <${OPSRAMP_CREDS_PATH} -r '."tracing-proxy".key')
+  SECRET=$(jq <${OPSRAMP_CREDS_PATH} -r '."tracing-proxy".secret')
+  TENANT_ID=$(jq <${OPSRAMP_CREDS_PATH} -r '."tracing-proxy".tenant_id')
 
   sed -i "s/<OPSRAMP_TRACES_API>/${TRACES_API}/g" ${TRACE_PROXY_CONFIG}
   sed -i "s/<OPSRAMP_METRICS_API>/${METRICS_API}/g" ${TRACE_PROXY_CONFIG}
