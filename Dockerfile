bef923b33176fe960870b44d1a2923fa71401262
lokesh.balla <lokesh.balla@hpe.com>
2023-09-12 06:45:34 +0530

    remove obselete metrics and reset gauge values every iteration

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,12 @@
 FROM --platform=$BUILDPLATFORM golang:alpine as builder
+
 ARG TARGETOS
 ARG TARGETARCH
 
-RUN apk update && apk add --no-cache git ca-certificates curl && update-ca-certificates
+RUN apk update && apk add --no-cache git bash ca-certificates && update-ca-certificates
 
-#Setting up tini
-ENV TINI_URL_ARM="https://coreupdate.central.arubanetworks.com/packages/tini-arm64"
-ENV TINI_ESUM_ARM="c3c8377b2b6bd62e8086be40ce967dd4a6910cec69b475992eff1800ec44b08e"
-ENV TINI_ESUM_AMD="57a120ebc06d16b3fae6a60b6b16da5a20711db41f8934c2089dea0d3eaa4f70"
-ENV TINI_URL_AMD="https://coreupdate.central.arubanetworks.com/packages/tini-amd64"
+ARG BUILD_ID="16.0.0"
 
-RUN set -eux; \
-        case "${TARGETARCH}" in \
-           aarch64|arm64) \
-             ESUM=$TINI_ESUM_ARM; \
-             BINARY_URL=$TINI_URL_ARM; \
-             ;; \
-           amd64|x86_64) \
-             ESUM=$TINI_ESUM_AMD; \
-             BINARY_URL=$TINI_URL_AMD; \
-             ;; \
-        esac; \
-        \
-    curl -fL -o /usr/local/bin/tini "${BINARY_URL}"; \
-    echo "${ESUM}  /usr/local/bin/tini" | sha256sum -c -; \
-    chmod +x /usr/local/bin/tini
-
-ARG BUILD_ID="15.0.0"
 WORKDIR /app
 
 ADD go.mod go.sum ./
@@ -43,22 +23,13 @@ RUN CGO_ENABLED=0 \
     -o tracing-proxy \
     ./cmd/tracing-proxy
 
-FROM --platform=$BUILDPLATFORM alpine:3.18
+FROM --platform=$TARGETPLATFORM alpine:3.18
 
-RUN apk update && apk add --no-cache bash jq ca-certificates && update-ca-certificates
+RUN apk update && apk add --no-cache bash ca-certificates && update-ca-certificates
 
 COPY --from=builder /app/config_complete.yaml /etc/tracing-proxy/config.yaml
 COPY --from=builder /app/rules_complete.yaml /etc/tracing-proxy/rules.yaml
 
 COPY --from=builder /app/tracing-proxy /usr/bin/tracing-proxy
-COPY --from=builder /usr/local/bin/tini /usr/local/bin/tini
 
-COPY --from=builder /app/start.sh /usr/bin/start.sh
-
-#ENTRYPOINT ["tini", \
-#            "-F", "/config/data/infra_elasticache.json", \
-#            "-F", "/config/data/infra_clusterinfo.json", \
-#            "-F", "/config/data/config_tracing-proxy.json", \
-#            "--"]
-
-CMD ["/usr/bin/start.sh"]
+CMD ["/usr/bin/tracing-proxy", "--config", "/etc/tracing-proxy/config.yaml", "--rules_config", "/etc/tracing-proxy/rules.yaml"]
---
+++ pkg/utils/utils_test.go
@@ -0,0 +1,168 @@
+package utils
+
+import (
+	"reflect"
+	"sync"
+	"testing"
+)
+
+func TestSyncedMap_Clear(t *testing.T) {
+	type testCase[K comparable, V any] struct {
+		name string
+		m    SyncedMap[K, V]
+	}
+	tests := []testCase[string, string]{
+		{
+			name: "clear",
+			m: SyncedMap[string, string]{
+				mu: sync.Mutex{},
+				m: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.m.Clear()
+			if len(tt.m.m) != 0 {
+				t.Errorf("failed to clear")
+			}
+		})
+	}
+}
+
+func TestSyncedMap_Delete(t *testing.T) {
+	type args[K comparable] struct {
+		key K
+	}
+	type testCase[K comparable, V any] struct {
+		name string
+		m    SyncedMap[K, V]
+		args args[K]
+		want map[K]V
+	}
+	tests := []testCase[string, string]{
+		{
+			name: "delete key 'test'",
+			m: SyncedMap[string, string]{
+				mu: sync.Mutex{},
+				m: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+			args: args[string]{
+				key: "key1",
+			},
+			want: map[string]string{
+				"key2": "value2",
+			},
+		},
+		{
+			name: "delete key 'test'",
+			m: SyncedMap[string, string]{
+				mu: sync.Mutex{},
+				m: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+			args: args[string]{
+				key: "key2",
+			},
+			want: map[string]string{
+				"key1": "value1",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.m.Delete(tt.args.key)
+			if !reflect.DeepEqual(tt.m.m, tt.want) {
+				t.Errorf("failed %v", tt.name)
+			}
+		})
+	}
+}
+
+func TestSyncedMap_Get(t *testing.T) {
+	type args[K comparable] struct {
+		key K
+	}
+	type testCase[K comparable, V any] struct {
+		name string
+		m    SyncedMap[K, V]
+		args args[K]
+		want V
+	}
+	tests := []testCase[string, string]{
+		{
+			name: "get key1",
+			m: SyncedMap[string, string]{
+				m: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+			args: args[string]{
+				key: "key1",
+			},
+			want: "value1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.Get(tt.args.key); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSyncedMap_Set(t *testing.T) {
+	type args[K comparable, V any] struct {
+		key   K
+		value V
+	}
+	type testCase[K comparable, V any] struct {
+		name string
+		m    SyncedMap[K, V]
+		args args[K, V]
+		want map[K]V
+	}
+	tests := []testCase[string, string]{
+		{
+			name: "add key1",
+			m: SyncedMap[string, string]{
+				m: map[string]string{},
+			},
+			args: args[string, string]{
+				key:   "key1",
+				value: "value1",
+			},
+			want: map[string]string{
+				"key1": "value1",
+			},
+		},
+		{
+			name: "add empty key",
+			m: SyncedMap[string, string]{
+				m: map[string]string{},
+			},
+			args: args[string, string]{
+				key:   "",
+				value: "value1",
+			},
+			want: map[string]string{
+				"": "value1",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.m.Set(tt.args.key, tt.args.value)
+		})
+	}
+}
