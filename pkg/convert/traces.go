c1711112c7aac27fc5acacaa621aefdb22828ff8
saikalyan.bhagavathula <kameswarasaikalyan.bhagavathula@hpe.com>
2024-02-26 12:22:42 +0530

    instance in span events

--- pkg/convert/traces.go
+++ pkg/convert/traces.go
@@ -245,10 +245,7 @@ func TranslateTraceRequest(request *coltracepb.ExportTraceServiceRequest, ri Req
 						traceAttributes["spanEventAttributes"]["traceId"] = traceID
 						traceAttributes["spanEventAttributes"]["spanId"] = spanID
 						traceAttributes["spanEventAttributes"]["trace_operation"] = span.Name
-						traceAttributes["spanEventAttributes"]["trace_instance"] = traceAttributes["spanAttributes"]["instance"]
-						if traceAttributes["spanEventAttributes"]["trace_instance"] == "" {
-							traceAttributes["spanEventAttributes"]["trace_instance"] = _unknown
-						}
+						traceAttributes["spanEventAttributes"]["trace_instance"] = traceAttributes["resourceAttributes"]["instance"]
 						traceAttributes["spanEventAttributes"]["trace_service"] = traceAttributes["resourceAttributes"]["service_name"]
 						spanEvents = append(spanEvents, SpanEvent{
 							Name:       sevent.GetName(),
